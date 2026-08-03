46601569e2f346edf3cf43a3134b466736463ac6
gna-king <wls0202dk@gmail.com>
2026-08-03 22:40:52 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+/*
+ * Firebase Console > 프로젝트 설정 > 내 앱 > SDK 설정 및 구성에서
+ * 아래 값을 그대로 복사해 넣으세요.
+ *
+ * 이 설정값은 웹 클라이언트 식별용이며 비밀번호가 아닙니다.
+ * 실제 데이터 보호는 database.rules.json의 보안 규칙이 담당합니다.
+ */
+window.WEDDING_FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBDMAKFNZIh3koMkY01SXHMMZJn4nqx1I8",
+    authDomain: "whoiscoming-8b749.firebaseapp.com",
+    databaseURL: "https://whoiscoming-8b749-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "whoiscoming-8b749",
+    storageBucket: "whoiscoming-8b749.firebasestorage.app",
+    messagingSenderId: "820733045882",
+    appId: "1:820733045882:web:675b02c1cdedfde44a5fc3"
+}; by
